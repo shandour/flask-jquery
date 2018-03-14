64f2a5c6289162c4b3219bbf0eb5a7d86b2f252d
shandour <shandour@lol.lol.lol>
2018-03-14 18:49:31 +0200

    minor fixed to the previous commit

--- projecto/frontend/__init__.py
+++ projecto/frontend/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-frontend_bp = Blueprint('frontend', __name__, template_folder='templates', url_prefix='/')
+frontend_bp = Blueprint('frontend', __name__, template_folder='templates')
